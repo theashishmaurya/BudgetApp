858b2f21d1c15b6ede5533c7cc841ea45c70b99e
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2020-07-18 16:19:42 +0530

    Controllers added

--- app.js
+++ app.js
@@ -12,8 +12,29 @@ var UiController = (function(){
 
 // Global App Controller
 var controller = (function(bgtCtrl , UiCtrl){
+
+    var ctrlAddItem = function(){
+        //console.log("Something happened");
+
+        // 1 . Get item from the input field 
+
+        // 2. Add item to buget contoller
+
+        // 3. Add item to UI 
+
+        // 4. Calculate the budget 
+        
+        // 5. Add the budget to the UI
+    }
     
-    document.querySelector
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress' , (event)=>{
+        if(event.keyCode === 13){
+            ctrlAddItem();
+        }
+        
+    })
     
 
 })(budgetController , UiController);
